deb5372f12d1a76253dec6f5a2d00ea3c5ce491f
Walker Malling <walker.malling@live.com>
2014-08-12 00:18:40 -0700

    add casperjs and first test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-casperjs');
 
   grunt.initConfig({
 
@@ -19,10 +20,20 @@ module.exports = function(grunt) {
           './app/style/master.compiled.css': './app/style/master.scss'
         }
       }
+    },
+
+    // CASPER
+    casperjs: {
+      options: {
+        async: {
+          parallel: false
+        }
+      },
+      files: ['test/acceptance/**/*.js']
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.registerTask('default', ['jshint','sass']);
+  grunt.registerTask('default', ['jshint','sass','casperjs']);
 };
