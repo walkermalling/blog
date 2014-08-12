02f5d21918dde631f8cd16fe275d8b5560da5d07
Walker Malling <walker.malling@live.com>
2014-08-11 23:52:18 -0700

    add sass dev dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,26 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.initConfig({
+
+    // JS HINT
     jshint: {
       all: ['Gruntfile.js', 'server.js']
+    },
+
+    // SASS
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {
+          './app/style/master.compiled.css': './app/style/master.scss'
+        }
+      }
     }
 
   });
 
-  grunt.registerTask('default','jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('default', ['jshint','sass']);
 };
